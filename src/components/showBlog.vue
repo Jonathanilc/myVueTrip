bdd2beff06549322374ca5e807f6384f56933e30
Jonathan <jonathanilc@hotmail.com>
2018-01-12 08:53:08 +1300

    This is my vue tutorial#36

---
+++ src/components/showBlog.vue
@@ -0,0 +1,80 @@
+<template>
+  <div v-theme:column="theme" id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <input type="text" v-model="search" placeholder="search blogs"/>
+    <div v-for="blog in filteredBlogs" class="single-blog">
+      <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
+      <article>{{blog.body | snippet}}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+
+  export default {
+
+
+
+    components:{
+
+
+    },
+    data () {
+      return {
+        blogs:[],
+
+        theme: 'narrow',
+
+        search: ""
+
+
+      }
+    },
+    methods:{
+      changeTheme(){
+        alert("clicked");
+        if(this.theme == 'narrow'){
+          this.theme = 'wide'
+          alert("changed to wide");
+        }else{
+          this.theme = 'narrow';
+        }
+      }
+    },
+
+
+    created(){
+      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function (data) {
+        console.log(data);
+        this.blogs = data.body.slice(0,10);
+      })
+    },
+
+    computed:{
+      filteredBlogs: function () {
+        return this.blogs.filter((blog)=>{
+          return blog.title.match(this.search);
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+
+  #show-blogs{
+    max-width:800px;
+    margin:0 auto;
+  }
+
+  .single-blog{
+    padding:20px;
+    margin:20px 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    background: #eee;
+  }
+</style>
